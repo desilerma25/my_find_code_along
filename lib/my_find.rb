f7a9c7d4a952ba7f5ef7bf0b3bf67135267bfce8
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-24 15:48:41 -0500

    commit tree 61f5044f166d6b43237f04a08e9d01ef4a8fc33e

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
-def my_find(collection)
+def my_find(collection) # def method that takes an arg.
+    i = 0 # begin counter at 0
+    while i < collection.length # only go up to the length of the array
+      if yield(collection[i]) # if block true, return arg. passed into yield (the element)
+        return collection[i]
+      end
+      i = i + 1 # otherwise cont. to the next element
+    end
+  end
 
-end
+
+  # refactor by putting the if state. all on one line using a statement modifier
+  def my_find(collection)
+    i = 0
+    while i < collection.length
+      return collection[i] if yield(collection[i])
+      i = i + 1
+    end
+  end 
+  
